77b485654d2323c0a732c6ca0f6e813cc27f8bb7
Terrance Hanlon <terrance.hanlon@ndsu.edu>
2017-11-10 19:38:12 -0600

    Add files via upload

--- JavaApplication1.java
+++ JavaApplication1.java
@@ -3,11 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package javaapplication1;
-import java.util.LinkedList;
-import java.util.Stack;
-import java.util.Queue;
+
 /**
  *
  * @author terrance.hanlon
@@ -17,68 +14,90 @@ public class JavaApplication1 {
     /**
      * @param args the command line arguments
      */
-   
-    public static boolean palindrome(String x){
-        int i = 0;
-        int p = 0;
-        boolean answer = false;
-        String s = x.toLowerCase();
-        String[] arr = s.split("");
-        Stack<String> stack = new Stack<String>();
-        Queue<String> queue = new LinkedList<>();
-        while(i < arr.length){
-            stack.push(arr[i]);
-            queue.add(arr[i]);
-            i++;
-        }
-       
-        for(int j = 0; j < arr.length; j++){
-          String stackPop = stack.pop();
-          String queuePop = queue.poll();
-       
-          if(!(stackPop.equals(queuePop)))
-                p = 0;
-            else
-                p = 1;
+     public static void sum(int[] x){
+        long[] arr = new long[x.length];
+        int min = Integer.MAX_VALUE;
+        
+        for(int i = 0; i < arr.length;i++){
+            for(int j = 0; j < arr.length; j++){
+                if(x[j] <= min){
+                   arr[i] = x[j];
+                   min = x[j];
+                }
             }
-        if(p == 1){
-            return true;
         }
-        else 
-        return false;
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
     }
     
     public static void main(String[] args) {
-        
-        String x = "Hello This Anna be false";
-        String[] y = x.split(" ");
-        boolean ans = false;
-        String text = "";
-        for(int i = 0; i < y.length; i++){
-            if(palindrome2(y[i])){
-                ans = true;
-                text += y[i];
-            }
-            
-        }
-        System.out.println(ans + " " + text);
-    }
-    
-    public static boolean palindrome2(String x){
-        boolean ans = false;
-        String s = x.toLowerCase();
-        String[] input = s.split("");
-        String rev = "";
-        for(int i = input.length-1; i >= 0; i--){
-            rev += input[i];
-        }
        
-        if(rev.equals(s)){
-            
-            return true;
+        int[] arr1 = new int[5];
+        arr1[0] = 6;
+        arr1[1] = 9;
+        arr1[2] = 10;
+        arr1[3] = 5;
+        arr1[4] = 7;
+        
+        
+        bubbleSort2(arr1);
+        //sum(arr1);
+    }
+
+   public static void bubbleSort(int[] array){
+       
+//    for(int i = 0; i < array.length-1-i;i++){
+//        for(int j = 0; j < array.length-1-i; j++){
+//            if(array[j] > array[j+1]){
+//                int temp = array[j+1];
+//                array[j] = array[j+1];
+//                array[j+1] = temp;
+//            }
+//        }
+//    }
+       
+  for (int i=0; i<array.length-1; i++) {
+  for (int j=0; j<array.length-1-i; j++)
+    if (array[j+1] < array[j]) {  /* compare the two neighbors */
+      int tmp = array[j];         /* swap a[j] and a[j+1]      */
+      array[j] = array[j+1];
+      array[j+1] = tmp;
+  }
+}
+
+//    for(int i = 0; i < array.length-1; i++){
+//        for(int j = 0; j < array.length-1-i; j++){
+//            if(array[j+1] < array[j]){
+//                int temp = array[j];
+//                array[j] = array[j+1];
+//                array[j+1] = temp;
+//            }
+//        }
+//    }
+    
+        for(int i = 0; i < array.length; i++){
+           System.out.println(array[i] + " ");
+       }
+   }
+ 
+
+
+public static void bubbleSort2(int[] x){
+    
+    for(int i = 0; i < x.length-1; i++){
+        for(int j = 0; j < x.length-1-i; j++){
+            if(x[j+1] < x[j]){
+                int temp = x[j];
+                x[j] = x[j+1];
+                x[j+1] = temp;
+            }
         }
-        else{
-           
-            return false;
-    }}
+    }
+      for(int i = 0; i < x.length; i++){
+           System.out.println(x[i] + " ");
+       }
+
+}
+   
 }
